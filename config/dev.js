668be9864626722a259631bb7aab6bd1c86590ad
mohdasaadeh <m.saadeh@wajeez.co>
2024-07-24 10:07:11 +0300

    deploy

--- config/dev.js
+++ config/dev.js
@@ -6,6 +6,4 @@ module.exports = {
     "mongodb://localhost:27017/advancedNode?retryWrites=true&w=majority",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379",
-  accessKeyId: "",
-  secretAccessKey: "",
 };
